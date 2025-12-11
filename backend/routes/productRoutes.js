968335662337788d5f4873b945b16f844de56e9b
Artan Rramanaj <artan.ramanaj@finnie.consulting>
2025-12-11 22:25:23 +0100

    generic routes last

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -14,8 +14,8 @@ const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route("/top").get(topProducts);
-router.route("/:id").get(getProduct);
 router.route("/:id/edit").put(protect, admin, editProduct);
 router.route("/:id/delete").delete(protect, admin, deleteProduct);
 router.route("/:id/reviews").post(protect, reviewProduct);
+router.route("/:id").get(getProduct);
 export default router;
